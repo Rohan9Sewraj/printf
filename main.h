5e7a4d6a58efb1ae415befc848d6c32a99112c31
Rohan Sewraj <sewrajrohan33@gmail.com>
2023-03-28 13:50:14 +0200

    HG

--- main.h
+++ main.h
@@ -15,30 +15,7 @@
 #define BUFF_SIZE 1024
 
 
-
-
 int print_float(va_list types, char buffer[], int flags, int width, int precision, int size);
-
-static fmt_t fmt_table[] = {
-    {'c', print_char},
-    {'s', print_string},
-    {'%', print_percent},
-    {'d', print_int},
-    {'i', print_int},
-    {'b', print_binary},
-    {'u', print_unsigned},
-    {'o', print_octal},
-    {'x', print_hexadecimal},
-    {'X', print_hexa_upper},
-    {'p', print_pointer},
-    {'S', print_non_printable},
-    {'r', print_reverse},
-    {'R', print_rot13string},
-    {'f', print_float},
-    {'\0', NULL}
-};
-
-
 
 
 
